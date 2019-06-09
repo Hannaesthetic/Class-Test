eb9ea521468371dd0b644d93646923607f3cd552
Hannah Rose <hannaesthetic.r@gmail.com>
2019-06-09 12:00:20 +0100

    GO DO A ONE HOUR GAME JAM

---
+++ Assets/Scripts/DrawingCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawingCamera : MonoBehaviour
+{
+    public GameObject[] pages;
+    public Camera drawingCamera;
+    public Transform canvasParent;
+
+    private RawImage overlayImage;
+    private int page;
+
+    void Start()
+    {
+        foreach (GameObject p in pages)
+        {
+            p.SetActive(false);
+        }
+        SetPage(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (page + 1 >= pages.Length)
+            {
+                //finish
+            } else
+            {
+                SetPage(page + 1);
+            }
+        }
+    }
+
+    void SetPage(int newPage)
+    {
+        if (overlayImage != null)
+        {
+            pages[page].GetComponent<Animator>().enabled = true;
+        }
+        page = newPage;
+        pages[page].SetActive(true);
+        overlayImage = pages[page].GetComponentInChildren<RawImage>();
+        overlayImage.enabled = true;
+        drawingCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        overlayImage.texture = drawingCamera.targetTexture;
+    }
+}
